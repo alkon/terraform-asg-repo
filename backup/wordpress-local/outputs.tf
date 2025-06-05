9d45a40ed9fc8f68249869cb4743a90353aedabc
alkon <aikbox@yahoo.com>
2025-06-05 15:37:36 +0300

    Remove backup folder

--- backup/wordpress-local/outputs.tf
+++
@@ -1,8 +0,0 @@
-# modules/wordpress-local/outputs.tf
-
-output "wordpress_url" {
-  description = "The URL to access the local WordPress instance."
-  # This value is derived from the input variable 'wordpress_port'
-  # which is passed into the module.
-  value = "http://localhost:${var.wordpress_port}"
-}
